3319563abc78affad8d70278085838267cb2e55b
Nicolas Mariniello <seldon1000@tutanota.com>
2022-04-05 12:40:22 +0200

    Cleanup

--- WinAnimation/Start.swift
+++ WinAnimation/Start.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct Start: View {
     @State var grid: Grid = Grid(level: level)
-    @State var showMenu: Bool = false
     @State var win: Bool = false
     
     var body: some View {
@@ -25,29 +24,13 @@ struct Start: View {
             }
             .environmentObject(grid)
             .contentShape(Rectangle())
-            .scaleEffect(showMenu ? 0.6 : 1.0)
-            .frame(width: UIScreen.main.bounds.width * (showMenu ? 0.6 : 1.0), height: UIScreen.main.bounds.height * (showMenu ? 0.6 : 1.0))
-            .onTapGesture {
-                withAnimation(.easeInOut) {
-                    showMenu = false
-                }
-            }
-            .onLongPressGesture {
-                if !win {
-                    withAnimation(.easeInOut) {
-                        showMenu = true
-                    }
-                }
-            }
             .gesture(DragGesture().onEnded { value in
-                if !win && !showMenu {
+                if !win {
                     if grid.dragGesture(translation: value.translation) {
                         win.toggle()
                         
                         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
                             withAnimation(.easeInOut) {
-                                showMenu = false
-                                
                                 grid.resetGrid()
                             }
                         }
@@ -55,6 +38,5 @@ struct Start: View {
                 }
             })
         }
-        .frame(width: UIScreen.main.bounds.width * (showMenu ? 0.6 : 1.0), height: UIScreen.main.bounds.height * (showMenu ? 0.6 : 1.0))
     }
 }
